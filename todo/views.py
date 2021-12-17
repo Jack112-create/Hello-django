58d4c6369639d38354d270bf1c9c482e259765a9
Jack Conroy <jackconroy777@gmail.com>
2021-12-17 13:58:29 +0000

    Fix edit_item view

--- todo/views.py
+++ todo/views.py
@@ -13,9 +13,9 @@ def get_todo_list(request):
 
 def add_item(request):
     if request.method == 'POST':
-        form = ItemForm(request.POST)
+        form  = ItemForm(request.POST)
         if form.is_valid():
-            form.save
+            form.save()
             return redirect('get_todo_list')
     form = ItemForm()
     context = {
@@ -26,9 +26,9 @@ def add_item(request):
 def edit_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     if request.method == 'POST':
-        form = ItemForm(request.POST, instance=item)
+        form  = ItemForm(request.POST, instance=item)
         if form.is_valid():
-            form.save
+            form.save()
             return redirect('get_todo_list')
     form = ItemForm(instance=item)
     context = {
